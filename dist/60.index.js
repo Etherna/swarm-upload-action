6b9fda9f9ab5b6e6a6c8e2f8837a8cbd9f04fed9
Mattia Dalzocchio <mattiadalzocchio@me.com>
2023-07-20 09:21:26 +0200

    build v2.0.0

---
+++ dist/60.index.js
@@ -0,0 +1,274 @@
+"use strict";
+exports.id = 60;
+exports.ids = [60];
+exports.modules = {
+
+/***/ 8060:
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "sha224": () => (/* binding */ S),
+/* harmony export */   "sha256": () => (/* binding */ G)
+/* harmony export */ });
+/* harmony import */ var _utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(3125);
+
+function m(x, t, n, s) {
+  if (typeof x.setBigUint64 == "function")
+    return x.setBigUint64(t, n, s);
+  const i = BigInt(32), f = BigInt(4294967295), e = Number(n >> i & f), h = Number(n & f), o = s ? 4 : 0, r = s ? 0 : 4;
+  x.setUint32(t + o, e, s), x.setUint32(t + r, h, s);
+}
+class L extends _utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.H {
+  constructor(t, n, s, i) {
+    super(), this.blockLen = t, this.outputLen = n, this.padOffset = s, this.isLE = i, this.finished = !1, this.length = 0, this.pos = 0, this.destroyed = !1, this.buffer = new Uint8Array(t), this.view = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.c)(this.buffer);
+  }
+  update(t) {
+    _utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.a.exists(this);
+    const { view: n, buffer: s, blockLen: i } = this;
+    t = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.t)(t);
+    const f = t.length;
+    for (let e = 0; e < f; ) {
+      const h = Math.min(i - this.pos, f - e);
+      if (h === i) {
+        const o = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.c)(t);
+        for (; i <= f - e; e += i)
+          this.process(o, e);
+        continue;
+      }
+      s.set(t.subarray(e, e + h), this.pos), this.pos += h, e += h, this.pos === i && (this.process(n, 0), this.pos = 0);
+    }
+    return this.length += t.length, this.roundClean(), this;
+  }
+  digestInto(t) {
+    _utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.a.exists(this), _utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.a.output(t, this), this.finished = !0;
+    const { buffer: n, view: s, blockLen: i, isLE: f } = this;
+    let { pos: e } = this;
+    n[e++] = 128, this.buffer.subarray(e).fill(0), this.padOffset > i - e && (this.process(s, 0), e = 0);
+    for (let c = e; c < i; c++)
+      n[c] = 0;
+    m(s, i - 8, BigInt(this.length * 8), f), this.process(s, 0);
+    const h = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.c)(t), o = this.outputLen;
+    if (o % 4)
+      throw new Error("_sha2: outputLen should be aligned to 32bit");
+    const r = o / 4, b = this.get();
+    if (r > b.length)
+      throw new Error("_sha2: outputLen bigger than state");
+    for (let c = 0; c < r; c++)
+      h.setUint32(4 * c, b[c], f);
+  }
+  digest() {
+    const { buffer: t, outputLen: n } = this;
+    this.digestInto(t);
+    const s = t.slice(0, n);
+    return this.destroy(), s;
+  }
+  _cloneInto(t) {
+    t || (t = new this.constructor()), t.set(...this.get());
+    const { blockLen: n, buffer: s, length: i, finished: f, destroyed: e, pos: h } = this;
+    return t.length = i, t.pos = h, t.finished = f, t.destroyed = e, i % n && t.buffer.set(s), t;
+  }
+}
+const _ = (x, t, n) => x & t ^ ~x & n, I = (x, t, n) => x & t ^ x & n ^ t & n, D = new Uint32Array([
+  1116352408,
+  1899447441,
+  3049323471,
+  3921009573,
+  961987163,
+  1508970993,
+  2453635748,
+  2870763221,
+  3624381080,
+  310598401,
+  607225278,
+  1426881987,
+  1925078388,
+  2162078206,
+  2614888103,
+  3248222580,
+  3835390401,
+  4022224774,
+  264347078,
+  604807628,
+  770255983,
+  1249150122,
+  1555081692,
+  1996064986,
+  2554220882,
+  2821834349,
+  2952996808,
+  3210313671,
+  3336571891,
+  3584528711,
+  113926993,
+  338241895,
+  666307205,
+  773529912,
+  1294757372,
+  1396182291,
+  1695183700,
+  1986661051,
+  2177026350,
+  2456956037,
+  2730485921,
+  2820302411,
+  3259730800,
+  3345764771,
+  3516065817,
+  3600352804,
+  4094571909,
+  275423344,
+  430227734,
+  506948616,
+  659060556,
+  883997877,
+  958139571,
+  1322822218,
+  1537002063,
+  1747873779,
+  1955562222,
+  2024104815,
+  2227730452,
+  2361852424,
+  2428436474,
+  2756734187,
+  3204031479,
+  3329325298
+]), u = new Uint32Array([
+  1779033703,
+  3144134277,
+  1013904242,
+  2773480762,
+  1359893119,
+  2600822924,
+  528734635,
+  1541459225
+]), d = new Uint32Array(64);
+class y extends L {
+  constructor() {
+    super(64, 32, 8, !1), this.A = u[0] | 0, this.B = u[1] | 0, this.C = u[2] | 0, this.D = u[3] | 0, this.E = u[4] | 0, this.F = u[5] | 0, this.G = u[6] | 0, this.H = u[7] | 0;
+  }
+  get() {
+    const { A: t, B: n, C: s, D: i, E: f, F: e, G: h, H: o } = this;
+    return [t, n, s, i, f, e, h, o];
+  }
+  // prettier-ignore
+  set(t, n, s, i, f, e, h, o) {
+    this.A = t | 0, this.B = n | 0, this.C = s | 0, this.D = i | 0, this.E = f | 0, this.F = e | 0, this.G = h | 0, this.H = o | 0;
+  }
+  process(t, n) {
+    for (let c = 0; c < 16; c++, n += 4)
+      d[c] = t.getUint32(n, !1);
+    for (let c = 16; c < 64; c++) {
+      const p = d[c - 15], l = d[c - 2], B = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(p, 7) ^ (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(p, 18) ^ p >>> 3, g = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(l, 17) ^ (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(l, 19) ^ l >>> 10;
+      d[c] = g + d[c - 7] + B + d[c - 16] | 0;
+    }
+    let { A: s, B: i, C: f, D: e, E: h, F: o, G: r, H: b } = this;
+    for (let c = 0; c < 64; c++) {
+      const p = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(h, 6) ^ (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(h, 11) ^ (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(h, 25), l = b + p + _(h, o, r) + D[c] + d[c] | 0, g = ((0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(s, 2) ^ (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(s, 13) ^ (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.r)(s, 22)) + I(s, i, f) | 0;
+      b = r, r = o, o = h, h = e + l | 0, e = f, f = i, i = s, s = l + g | 0;
+    }
+    s = s + this.A | 0, i = i + this.B | 0, f = f + this.C | 0, e = e + this.D | 0, h = h + this.E | 0, o = o + this.F | 0, r = r + this.G | 0, b = b + this.H | 0, this.set(s, i, f, e, h, o, r, b);
+  }
+  roundClean() {
+    d.fill(0);
+  }
+  destroy() {
+    this.set(0, 0, 0, 0, 0, 0, 0, 0), this.buffer.fill(0);
+  }
+}
+class E extends y {
+  constructor() {
+    super(), this.A = -1056596264, this.B = 914150663, this.C = 812702999, this.D = -150054599, this.E = -4191439, this.F = 1750603025, this.G = 1694076839, this.H = -1090891868, this.outputLen = 28;
+  }
+}
+const G = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.w)(() => new y()), S = (0,_utils_26d2af60_mjs__WEBPACK_IMPORTED_MODULE_0__.w)(() => new E());
+
+//# sourceMappingURL=sha256-2ef38056.mjs.map
+
+
+/***/ }),
+
+/***/ 3125:
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "H": () => (/* binding */ g),
+/* harmony export */   "a": () => (/* binding */ l),
+/* harmony export */   "c": () => (/* binding */ h),
+/* harmony export */   "r": () => (/* binding */ d),
+/* harmony export */   "t": () => (/* binding */ y),
+/* harmony export */   "w": () => (/* binding */ E)
+/* harmony export */ });
+function r(t) {
+  if (!Number.isSafeInteger(t) || t < 0)
+    throw new Error(`Wrong positive integer: ${t}`);
+}
+function f(t) {
+  if (typeof t != "boolean")
+    throw new Error(`Expected boolean, not ${t}`);
+}
+function o(t, ...e) {
+  if (!(t instanceof Uint8Array))
+    throw new Error("Expected Uint8Array");
+  if (e.length > 0 && !e.includes(t.length))
+    throw new Error(`Expected Uint8Array of length ${e}, not of length=${t.length}`);
+}
+function s(t) {
+  if (typeof t != "function" || typeof t.create != "function")
+    throw new Error("Hash should be wrapped by utils.wrapConstructor");
+  r(t.outputLen), r(t.blockLen);
+}
+function c(t, e = !0) {
+  if (t.destroyed)
+    throw new Error("Hash instance has been destroyed");
+  if (e && t.finished)
+    throw new Error("Hash#digest() has already been called");
+}
+function u(t, e) {
+  o(t);
+  const n = e.outputLen;
+  if (t.length < n)
+    throw new Error(`digestInto() expects output buffer of length at least ${n}`);
+}
+const l = {
+  number: r,
+  bool: f,
+  bytes: o,
+  hash: s,
+  exists: c,
+  output: u
+};
+/*! noble-hashes - MIT License (c) 2022 Paul Miller (paulmillr.com) */
+const a = (t) => t instanceof Uint8Array, h = (t) => new DataView(t.buffer, t.byteOffset, t.byteLength), d = (t, e) => t << 32 - e | t >>> e, w = new Uint8Array(new Uint32Array([287454020]).buffer)[0] === 68;
+if (!w)
+  throw new Error("Non little-endian hardware is not supported");
+Array.from({ length: 256 }, (t, e) => e.toString(16).padStart(2, "0"));
+function p(t) {
+  if (typeof t != "string")
+    throw new Error(`utf8ToBytes expected string, got ${typeof t}`);
+  return new Uint8Array(new TextEncoder().encode(t));
+}
+function y(t) {
+  if (typeof t == "string" && (t = p(t)), !a(t))
+    throw new Error(`expected Uint8Array, got ${typeof t}`);
+  return t;
+}
+class g {
+  // Safe version that clones internal state
+  clone() {
+    return this._cloneInto();
+  }
+}
+function E(t) {
+  const e = (i) => t().update(y(i)).digest(), n = t();
+  return e.outputLen = n.outputLen, e.blockLen = n.blockLen, e.create = () => t(), e;
+}
+
+//# sourceMappingURL=utils-26d2af60.mjs.map
+
+
+/***/ })
+
+};
+;
